6b1c4672683397efe27a6d6e20a4cb080442ccb9
Nasirudeen Olawale <gemdajs@gmail.com>
2025-08-11 18:03:22 +0100

    Fix: Loan extention due date signal

--- library/signals.py
+++ library/signals.py
@@ -11,6 +11,7 @@ from library.models import Loan
 def update_due_date_before_save(sender, instance, **kwargs):
     if instance.due_date is None:
         due_date = instance.loan_date
+        print("Due date", due_date)
         if due_date is None:
             due_date = timezone.now()
         due_date = due_date + timedelta(days=14)
